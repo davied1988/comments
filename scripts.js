30612d7c9f76757a8808901cd6c00b59a9634dd7
Dawid G <dawid.grudziecki@gmail.com>
2017-11-08 03:06:50 +0100

    comments in js files

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+
+// alert('Hello, ' + name);
+
+console.log('Hello, ' + name);
+
+/*
+var doubt = "I love you";
+var doubtHim = "I love you not";
+*/
+
+// cooment added for pull request
